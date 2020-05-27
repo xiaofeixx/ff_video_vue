beab1218822e3dad70ac107536b1d3657f362bdd
xiaofeixx <1346574090@qq.com>
2020-05-27 16:44:46 +0800

    '对头组件进一步完善'

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,31 @@
              <el-col :xl="2" :lg="2" :md="2" class="hidden-sm-and-down"><el-menu-item index="6"><a href="https://www.ele.me" target="_blank">动漫</a></el-menu-item></el-col>
              <el-col :xl="2" :lg="2" :md="2" class="hidden-sm-and-down" ><el-menu-item index="7"><a href="https://www.ele.me" target="_blank">专题</a></el-menu-item></el-col>
              <el-col :xl="2" :lg="2"  class="hidden-md-and-down"> <el-menu-item index="8"><a href="https://www.ele.me" target="_blank">明星</a></el-menu-item></el-col>
-             <el-col :xl="2" :lg="2" :md="2" :sm="3" :xs="4"><el-menu-item index="9"><a href="https://www.ele.me" target="_blank">频道</a></el-menu-item></el-col>
+             <el-col :xl="2" :lg="2" :md="2" :sm="3" :xs="4"><el-menu-item index="9"><a target="_blank">
+               <el-dropdown @command="handleCommand">
+  <span class="el-dropdown-link">
+    频道<i class="el-icon-arrow-down el-icon--right"></i>
+  </span>
+                 <el-dropdown-menu slot="dropdown">
+                   <el-card class="box-card" :body-style="{width: cardWidth}">
+                    <el-row :gutter="20">
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电影</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                      <el-col :xl="4" :lg="4" :md="4" :sm="6" :xs="6"><el-button>电视剧</el-button></el-col>
+                    </el-row>
+                   </el-card>
+                 </el-dropdown-menu>
+               </el-dropdown>
+             </a></el-menu-item></el-col>
              <el-col :xl="4" :lg="4" :md="4" :sm="7" class="hidden-xs-only">
                <el-menu-item index="10">
                  <el-input
@@ -61,6 +85,7 @@ export default {
   data () {
     return {
       dialogWidth: 0,
+      cardWidth: 0,
       url: require('../assets/image/logo1.png'),
       gridData: [{
         date: '2016-05-02',
@@ -104,8 +129,10 @@ export default {
       const def = 400 // 默认宽度
       if (val < def) {
         this.dialogWidth = '100%'
+        this.cardWidth = '100%'
       } else {
         this.dialogWidth = def + 'px'
+        this.cardWidth = (def + 100) + 'px'
       }
     }
   }
